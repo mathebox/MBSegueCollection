eb15e5350a764ed417b1d4869b854d1556a7c95b
Max Bothe <mathebox@gmail.com>
2015-01-01 17:08:17 +0100

    Add demo project.

---
+++ MBSegueDemo/MBSegueDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBSegueDemo
+//
+//  Created by Max Bothe on 01/01/15.
+//  Copyright (c) 2015 Max Bothe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBSegueDemo/MBSegueDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBSegueDemo
+//
+//  Created by Max Bothe on 01/01/15.
+//  Copyright (c) 2015 Max Bothe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
